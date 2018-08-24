95ce0da9e4a6abb88ba88666266183013720f09f
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-08-24 16:47:06 -0300

    feat: Implemented the counter app.

--- counter/src/index.js
+++ counter/src/index.js
@@ -1,23 +1,57 @@
-import h from 'hyperscript';
 import hh from 'hyperscript-helpers';
+import { diff, h, patch } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
 
 const { div, button } = hh(h);
 
 const initModel = 0;
 
-function view(model) {
+function view(dispatch, model) {
   return div([
     div({ className: 'mv2' }, `Count: ${model}`),
     button(
-      { className: 'pv1 ph2 mr2', onclick: () => console.log('+ clicked') },
+      { className: 'pv1 ph2 mr2', onclick: () => dispatch(MSGS.ADD) },
       '+'
     ),
     button(
-      { className: 'pv1 ph2', onclick: () => console.log('- clicked') },
+      { className: 'pv1 ph2', onclick: () => dispatch(MSGS.SUBTRACT) },
       '-'
     )
   ]);
 }
 
+const MSGS = {
+  ADD: 'ADD',
+  SUBTRACT: 'SUBTRACT'
+};
+
+function update(msg, model) {
+  switch (msg) {
+    case MSGS.ADD:
+      return model + 1;
+    case MSGS.SUBTRACT:
+      return model - 1;
+    default:
+      return model;
+  }
+}
+
+// Impure code below
+function app(initModel, update, view, node) {
+  let model = initModel;
+  let currentView = view(dispatch, model);
+  let rootNode = createElement(currentView);
+  node.appendChild(rootNode);
+
+  function dispatch(msg) {
+    model = update(msg, model);
+    const updatedView = view(dispatch, model);
+    const patches = diff(currentView, updatedView);
+    rootNode = patch(rootNode, patches);
+    currentView = updatedView;
+  }
+}
+
 const rootNode = document.getElementById('app');
-rootNode.appendChild(view(initModel));
+
+app(initModel, update, view, rootNode);
