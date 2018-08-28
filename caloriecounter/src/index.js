fa368bd609a64c4b1d1d66cc4b29110b69856c5c
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-08-28 11:11:46 -0300

    feat: Add the initial functions for the calories app.

--- caloriecounter/src/index.js
+++ caloriecounter/src/index.js
@@ -1,1 +1,8 @@
-console.log('It Worked!');
+import app from './App';
+import initModel from './model';
+import update from './update';
+import view from './view';
+
+const node = document.getElementById('app');
+
+app(initModel, update, view, node);
