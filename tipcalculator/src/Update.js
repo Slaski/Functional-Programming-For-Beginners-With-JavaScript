e0b47096992a02a0ce636875e96bbf308fdfb390
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-08-31 14:17:49 -0300

    feat: Add the update functions for the bill and tipPercentage.

--- tipcalculator/src/Update.js
+++ tipcalculator/src/Update.js
@@ -1,6 +1,33 @@
 import * as R from 'ramda';
 
-function update (msg, model) {
+const MSG = {
+  BILL_CHANGED: 'BILL_CHANGED',
+  TIP_CHANGED: 'TIP_CHANGED'
+};
+
+export function billChangedMsg(bill) {
+  return { type: MSG.BILL_CHANGED, bill };
+}
+
+export function tipChangedMsg(tip) {
+  return { type: MSG.TIP_CHANGED, tip };
+}
+
+const toFloat = R.pipe(
+  parseFloat,
+  R.defaultTo(0.0)
+);
+
+function update(msg, model) {
+  switch (msg.type) {
+    case MSG.BILL_CHANGED: {
+      return { ...model, bill: toFloat(msg.bill) };
+    }
+    case MSG.TIP_CHANGED: {
+      return { ...model, tipPercentage: toFloat(msg.tip) };
+    }
+  }
+
   return model;
 }
 
--- tipcalculator/src/View.js
+++ tipcalculator/src/View.js
@@ -1,15 +1,17 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import { billChangedMsg, tipChangedMsg } from './Update';
 
 const { div, h1, pre, input, label } = hh(h);
 
-function inputSection(labelText, value) {
+function inputSection(labelText, value, changeFn) {
   return div([
     label({ className: 'db m1' }, labelText),
     input({
       type: 'text',
       className: 'db w-100 mv2 pa2 input-reset ba',
-      value
+      value,
+      oninput: e => changeFn(e.target.value)
     })
   ]);
 }
@@ -21,8 +23,12 @@ function resultSection(labelText, value) {
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Tip Calculator'),
-    inputSection('Bill Amount', model.bill || ''),
-    inputSection('Tip %', model.tipPercentage || ''),
+    inputSection('Bill Amount', model.bill || '', val =>
+      dispatch(billChangedMsg(val))
+    ),
+    inputSection('Tip %', model.tipPercentage || '', val =>
+      dispatch(tipChangedMsg(val))
+    ),
     resultSection('Tip', model.tip),
     resultSection('Total', model.total),
     pre(JSON.stringify(model, null, 2))
