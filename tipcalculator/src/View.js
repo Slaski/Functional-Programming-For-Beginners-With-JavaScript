9ba0d5932cde7f9e72df88c1815f4d10cfa6a1bd
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-08-31 12:00:59 -0300

    feat: Add the view functions and the initial model.

--- tipcalculator/src/View.js
+++ tipcalculator/src/View.js
@@ -1,18 +1,31 @@
-import * as R from 'ramda';
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
-const {
-  div,
-  h1,
-  pre,
-} = hh(h);
+const { div, h1, pre, input, label } = hh(h);
 
+function inputSection(labelText, value) {
+  return div([
+    label({ className: 'db m1' }, labelText),
+    input({
+      type: 'text',
+      className: 'db w-100 mv2 pa2 input-reset ba',
+      value
+    })
+  ]);
+}
+
+function resultSection(labelText, value) {
+  return div([label({ className: 'db m1' }, `${labelText}: $${value}`)]);
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Tip Calculator'),
-    pre(JSON.stringify(model, null, 2)),
+    inputSection('Bill Amount', model.bill || ''),
+    inputSection('Tip %', model.tipPercentage || ''),
+    resultSection('Tip', model.tip),
+    resultSection('Total', model.total),
+    pre(JSON.stringify(model, null, 2))
   ]);
 }
 
